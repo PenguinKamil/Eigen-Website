5a5d3b33165fc4169f730ad21369a402bbb20613
Kamil Marsman <97417228+PenguinKamil@users.noreply.github.com>
2022-01-27 15:55:29 +0000

    Veel veranderd

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-var i = 0;
-var txt = 'Lorem ipsum typing effect!'; /* The text */
+var I = 0;
+var txt = 'Holman, T. (2017). Een dictator? Je krijgt er een heerlijk leven voor terug. Het Parool.'; /* The text */
 var speed = 50; /* The speed/duration of the effect in milliseconds */
  
 function typeWriter() {
- if (i < txt.length) {
-   document.getElementById("demo").innerHTML += txt.charAt(i);
-   i++;
+ if (I < txt.length) {
+   document.getElementById("demo").innerHTML += txt.charAt(I);
+   I++;
    setTimeout(typeWriter, speed);
  }
 }
